742c69b633e2c5284e454bba29ff9587a84ccde9
Pupkin Dima <dimasav57@gmail.com>
2021-07-28 18:49:43 +0300

    added search steam install path method and test

---
+++ DotaDiscordExtension.Core/SteamSearcher.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Win32;
+
+namespace DotaDiscordExtension.Core
+{
+    public class SteamSearcher
+    {
+        public string FindSteamInstallPath()
+        {
+            const string defaultPath32Bit = @"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam";
+            const string defaultPath64Bit = @"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam";
+
+            return (string)Registry.GetValue(defaultPath32Bit, "InstallPath", null)
+                   ?? (string)Registry.GetValue(defaultPath64Bit, "InstallPath", null);
+        }
+    }
+}
---
+++ DotaDiscordExtension.Tests/SteamSearcherTests.cs
@@ -0,0 +1,25 @@
+﻿using DotaDiscordExtension.Core;
+using Xunit;
+
+namespace DotaDiscordExtension.Tests
+{
+    public class SteamSearcherTests
+    {
+        private readonly SteamSearcher _steam;
+
+        public SteamSearcherTests()
+        {
+            _steam = new SteamSearcher();
+        }
+
+        [Fact]
+        public void IsPathsMatch_ValuesCoincide_ReturnTrue()
+        {
+            var expected = @"D:\steam";
+
+            var actual = _steam.FindSteamInstallPath();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}

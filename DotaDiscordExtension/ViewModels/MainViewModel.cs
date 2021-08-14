c7b8498118fc85414664cfb56f84bbb299045f50
Pupkin Dima <dimasav57@gmail.com>
2021-08-14 10:23:55 +0300

    new func and notifyIcon created

---
+++ DotaDiscordExtension/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using DotaDiscordExtension.UI.Infrastructure.Commands;
+using DotaDiscordExtension.UI.Resources;
+using DotaDiscordExtension.UI.ViewModels.Base;
+
+namespace DotaDiscordExtension.UI.ViewModels
+{
+    public class MainViewModel : ViewModel
+    {
+        private ICommand _closeCommand;
+        public ICommand CloseCommand => _closeCommand ?? new LambdaCommand(e =>
+        {
+            if (Application.Current.MainWindow != null) Application.Current.MainWindow.Hide();
+            ((CancelEventArgs) e).Cancel = true;
+        });
+    }
+}
